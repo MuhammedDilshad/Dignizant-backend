0f17d984d5cebcba07cd99c24a5b323fcb2f49a5
dilshad <md.mdsar@gmail.com>
2024-01-11 18:16:35 +0530

    code updated

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,9 @@
 import express from "express";
 
 const router = express.Router();
-// import { formController } from "../controllers/userController.js";
-// user.js
+
 import formController from "../controllers/userController.js";
 
 router.post("/googleForm", formController);
-
-// router.post("/googleForm", user);
 
 export default router;
